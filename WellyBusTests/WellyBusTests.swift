5b432de0957dabf950385a91dfe5d98547a54cb7
Eoin Kelly <eoin.kelly@ackama.com>
2024-10-26 07:51:27 +1300

    Initial Commit

---
+++ WellyBusTests/WellyBusTests.swift
@@ -0,0 +1,17 @@
+//
+//  WellyBusTests.swift
+//  WellyBusTests
+//
+//  Created by Eoin Kelly on 26/10/2024.
+//
+
+import Testing
+@testable import WellyBus
+
+struct WellyBusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
